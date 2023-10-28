f1fa2749fea17279e6f71463bc981dcb2101d3b2
vikrambarr <37145312+vikrambarr@users.noreply.github.com>
2023-10-28 17:35:34 +0100

    test compounded learnsets

---
+++ data/mods/gen9infinitefusion/learnsets.ts
@@ -0,0 +1,143 @@
+const baseLearnsets = require('../../learnsets').Learnsets;
+export const Learnsets: {[k: string]: ModdedLearnsetData} = {
+	bulbmantle: {
+		learnset: {
+			...baseLearnsets.bulbasaur.learnset,
+			...baseLearnsets.charmander.learnset,
+			...baseLearnsets.venusaur.learnset,
+		}
+	},
+	ivymelortle: {
+		learnset: {
+			...baseLearnsets.bulbasaur.learnset,
+			...baseLearnsets.charmander.learnset,
+			...baseLearnsets.venusaur.learnset,
+		}
+	},
+	venustoizard: {
+		learnset: {
+			...baseLearnsets.bulbasaur.learnset,
+			...baseLearnsets.charmander.learnset,
+			...baseLearnsets.venusaur.learnset,
+		}
+	},
+	totoritaquil: {
+		learnset: {
+			...baseLearnsets.bulbasaur.learnset,
+			...baseLearnsets.charmander.learnset,
+			...baseLearnsets.venusaur.learnset,
+		}
+	},
+	baylavanaw: {
+		learnset: {
+			...baseLearnsets.bulbasaur.learnset,
+			...baseLearnsets.charmander.learnset,
+			...baseLearnsets.venusaur.learnset,
+		}
+	},
+	megaligasion: {
+		learnset: {
+			...baseLearnsets.bulbasaur.learnset,
+			...baseLearnsets.charmander.learnset,
+			...baseLearnsets.venusaur.learnset,
+		}
+	},
+	torkipcko: {
+		learnset: {
+			...baseLearnsets.bulbasaur.learnset,
+			...baseLearnsets.charmander.learnset,
+			...baseLearnsets.venusaur.learnset,
+		}
+	},
+	gromarshken: {
+		learnset: {
+			...baseLearnsets.bulbasaur.learnset,
+			...baseLearnsets.charmander.learnset,
+			...baseLearnsets.venusaur.learnset,
+		}
+	},
+	swamptiliken: {
+		learnset: {
+			...baseLearnsets.bulbasaur.learnset,
+			...baseLearnsets.charmander.learnset,
+			...baseLearnsets.venusaur.learnset,
+		}
+	},
+	turcharlup: {
+		learnset: {
+			...baseLearnsets.bulbasaur.learnset,
+			...baseLearnsets.charmander.learnset,
+			...baseLearnsets.venusaur.learnset,
+		}
+	},
+	prinfernotle: {
+		learnset: {
+			...baseLearnsets.bulbasaur.learnset,
+			...baseLearnsets.charmander.learnset,
+			...baseLearnsets.venusaur.learnset,
+		}
+	},
+	torterneon: {
+		learnset: {
+			...baseLearnsets.bulbasaur.learnset,
+			...baseLearnsets.charmander.learnset,
+			...baseLearnsets.venusaur.learnset,
+		}
+	},
+	zapmolticuno: {
+		learnset: {
+			...baseLearnsets.bulbasaur.learnset,
+			...baseLearnsets.charmander.learnset,
+			...baseLearnsets.venusaur.learnset,
+		}
+	},
+	enraicune: {
+		learnset: {
+			...baseLearnsets.bulbasaur.learnset,
+			...baseLearnsets.charmander.learnset,
+			...baseLearnsets.venusaur.learnset,
+		}
+	},
+	kyodonquaza: {
+		learnset: {
+			...baseLearnsets.bulbasaur.learnset,
+			...baseLearnsets.charmander.learnset,
+			...baseLearnsets.venusaur.learnset,
+		}
+	},
+	paldiatina: {
+		learnset: {
+			...baseLearnsets.bulbasaur.learnset,
+			...baseLearnsets.charmander.learnset,
+			...baseLearnsets.venusaur.learnset,
+		}
+	},
+	zekyushiram: {
+		learnset: {
+			...baseLearnsets.bulbasaur.learnset,
+			...baseLearnsets.charmander.learnset,
+			...baseLearnsets.venusaur.learnset,
+		}
+	},
+	celemewchi: {
+		learnset: {
+			...baseLearnsets.bulbasaur.learnset,
+			...baseLearnsets.charmander.learnset,
+			...baseLearnsets.venusaur.learnset,
+		}
+	},
+	regitrio: {
+		learnset: {
+			...baseLearnsets.bulbasaur.learnset,
+			...baseLearnsets.charmander.learnset,
+			...baseLearnsets.venusaur.learnset,
+		}
+	},
+	deosectwo: {
+		learnset: {
+			...baseLearnsets.bulbasaur.learnset,
+			...baseLearnsets.charmander.learnset,
+			...baseLearnsets.venusaur.learnset,
+		}
+	},
+};
