afa1ceab5405671caed5404ed206a3d09346eb81
vikrambarr <37145312+vikrambarr@users.noreply.github.com>
2023-11-08 12:49:24 +0000

    untested formeChange code for preserving fusion

--- data/mods/gen9infinitefusion/scripts.ts
+++ data/mods/gen9infinitefusion/scripts.ts
@@ -109,5 +109,67 @@ export const Scripts: ModdedBattleScriptsData = {
 	
 			return true;
 		},
+		formeChange(speciesId, source, isPermanent, message) {
+			const rawSpecies = this.battle.dex.species.get(speciesId);
+	
+			const species = this.setSpecies(rawSpecies, source);
+			if (!species) return false;
+	
+			if (this.battle.gen <= 2) return true;
+	
+			// The species the opponent sees
+			const apparentSpecies =
+				this.illusion ? this.illusion.species.name : species.baseSpecies;
+			if (isPermanent) {
+				this.baseSpecies = rawSpecies;
+				this.details = species.name + (this.level === 100 ? '' : ', L' + this.level) +
+					(this.gender === '' ? '' : ', ' + this.gender) + (this.set.shiny ? ', shiny' : '') +
+					(this.set.fusion ? ', fusion: ' + this.set.fusion : '');
+				let details = (this.illusion || this).details;
+				if (this.terastallized) details += `, tera:${this.terastallized}`;
+				this.battle.add('detailschange', this, details);
+				if (source.effectType === 'Item') {
+					this.canTerastallize = null; // National Dex behavior
+					if (source.zMove) {
+						this.battle.add('-burst', this, apparentSpecies, species.requiredItem);
+						this.moveThisTurnResult = true; // Ultra Burst counts as an action for Truant
+					} else if (source.onPrimal) {
+						if (this.illusion) {
+							this.ability = '';
+							this.battle.add('-primal', this.illusion, species.requiredItem);
+						} else {
+							this.battle.add('-primal', this, species.requiredItem);
+						}
+					} else {
+						// So a Mega Evolution message isn't sent while we're waiting on Ogerpon text
+						if (source.megaEvolves) {
+							this.battle.add('-mega', this, apparentSpecies, species.requiredItem);
+						}
+						this.moveThisTurnResult = true; // Mega Evolution counts as an action for Truant
+					}
+				} else if (source.effectType === 'Status') {
+					// Shaymin-Sky -> Shaymin
+					this.battle.add('-formechange', this, species.name, message);
+				}
+			} else {
+				if (source.effectType === 'Ability') {
+					this.battle.add('-formechange', this, species.name, message, `[from] ability: ${source.name}`);
+				} else {
+					this.battle.add('-formechange', this, this.illusion ? this.illusion.species.name : species.name, message);
+				}
+			}
+			if (isPermanent && !['disguise', 'iceface'].includes(source.id)) {
+				if (this.illusion) {
+					this.ability = ''; // Don't allow Illusion to wear off
+				}
+				this.setAbility(species.abilities['0'], null, true);
+				this.baseAbility = this.ability;
+			}
+			if (this.terastallized) {
+				this.knownType = true;
+				this.apparentType = this.terastallized;
+			}
+			return true;
+		}
 	},
 };
