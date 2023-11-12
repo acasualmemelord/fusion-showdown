397cd992cf9c935d073f44c274ce378774526195
vikrambarr <37145312+vikrambarr@users.noreply.github.com>
2023-11-12 15:36:27 +0000

    legalize missing moves from cool additions

--- data/mods/gen7infinitefusion/moves.ts
+++ data/mods/gen7infinitefusion/moves.ts
@@ -1,2 +1,24 @@
 const {Dex} = require('../../../sim/dex');
-export const Moves: {[k: string]: ModdedMoveData} = Dex.deepClone(require('../gen9infinitefusion/moves').Moves);
+export const Moves: {[k: string]: ModdedMoveData} = {
+	lightofruin: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	geomancy: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	oblivionwing: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	moongeistbeam: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	doubleironbash: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	...Dex.deepClone(require('../gen9infinitefusion/moves').Moves),
+};
