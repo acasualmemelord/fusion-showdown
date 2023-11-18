9b71e8ba86e065be3b878d26fb4a96a088fe8218
vikrambarr <37145312+vikrambarr@users.noreply.github.com>
2023-11-18 13:19:19 +0000

    likely wonderguard fix

--- data/rulesets.ts
+++ data/rulesets.ts
@@ -2728,7 +2728,7 @@ export const Rulesets: {[k: string]: FormatData} = {
 				newSpecies.bst += newSpecies.baseStats[stat];
 			}
 
-			if (target.baseAbility === 'wonderguard') newSpecies.maxHP = 1;
+			newSpecies.maxHP = target.baseAbility === 'wonderguard' ? 1 : undefined;
 			newSpecies.weightkg = (fusionSpecies.weightkg + species.weightkg) / 2;
 			newSpecies.weighthg = newSpecies.weightkg * 10;
 
