902c6bbe441c3f4f78db2b27dd7ebd1c0f2dc3a7
vikrambarr <37145312+vikrambarr@users.noreply.github.com>
2024-02-23 13:35:40 +0000

    hide pokeathlon temporarily

--- config/alt-formats.ts
+++ config/alt-formats.ts
@@ -163,18 +163,18 @@ export const Formats: FormatList = [
 		// no restrictions, for serious (other than team preview)
 		ruleset: ['Team Preview', 'Cancel Mod', 'Max Team Size = 24', 'Max Move Count = 24', 'Max Level = 9999', 'Default Level = 100', 'Infinite Fusion Mod'],
 	},
-	{
-		section: "Pokéathlon",
-		column: 2,
-	},
-	{
-		name: "[Gen 9] Pokeathlon Custom Game",
+	// {
+	// 	section: "Pokéathlon",
+	// 	column: 2,
+	// },
+	// {
+	// 	name: "[Gen 9] Pokeathlon Custom Game",
 
-		mod: 'gen9pokeathlon',
-		searchShow: false,
-		debug: true,
-		battle: {trunc: Math.trunc},
-		// no restrictions, for serious (other than team preview)
-		ruleset: ['Team Preview', 'Cancel Mod', 'Max Team Size = 24', 'Max Move Count = 24', 'Max Level = 9999', 'Default Level = 100'],
-	},
+	// 	mod: 'gen9pokeathlon',
+	// 	searchShow: false,
+	// 	debug: true,
+	// 	battle: {trunc: Math.trunc},
+	// 	// no restrictions, for serious (other than team preview)
+	// 	ruleset: ['Team Preview', 'Cancel Mod', 'Max Team Size = 24', 'Max Move Count = 24', 'Max Level = 9999', 'Default Level = 100'],
+	// },
 ];
