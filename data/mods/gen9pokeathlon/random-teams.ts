4e563a38d513c9f9f000e23d722299ad5a5463e8
vikrambarr <37145312+vikrambarr@users.noreply.github.com>
2024-02-25 01:51:49 +0000

    update randbats

--- data/mods/gen9pokeathlon/random-teams.ts
+++ data/mods/gen9pokeathlon/random-teams.ts
@@ -53,7 +53,7 @@ export class RandomGen7Teams extends RandomGen8Teams {
 				}
 			}
 
-			if (curSet.moves && curSet.ability && curSet.item) {
+			if (curSet.moves && curSet.ability && curSet.item && curSet.teraType) {
 				pokemon.push({
 					name: this.dex.species.get(curSpecies).name,
 					species: this.dex.species.get(curSpecies).name,
@@ -64,6 +64,7 @@ export class RandomGen7Teams extends RandomGen8Teams {
 					moves: curSet.moves,
 					ability: curSet.ability,
 					item: curSet.item,
+					teraType: curSet.teraType,
 					evs: {hp: 84, atk: 84, def: 84, spa: 84, spd: 84, spe: 84},
 					ivs: {hp: 31, atk: 31, def: 31, spa: 31, spd: 31, spe: 31},
 				});
