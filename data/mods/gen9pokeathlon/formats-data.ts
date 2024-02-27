6b62ef994bc0bf1b22fe049eab1feabc18d5a7d5
vikrambarr <37145312+vikrambarr@users.noreply.github.com>
2024-02-27 02:22:50 +0000

    work?

--- data/mods/gen9pokeathlon/formats-data.ts
+++ data/mods/gen9pokeathlon/formats-data.ts
@@ -1,372 +1,298 @@
 export const FormatsData: {[k: string]: ModdedSpeciesFormatsData} = {
 	syclar: {
 		inherit: true,
-		isNonstandard: null,
 		tier: "LC",
 	},
 	syclant: {
 		inherit: true,
-		isNonstandard: null,
 		tier: "OU",
 	},
 	revenankh: {
 		inherit: true,
-		isNonstandard: null,
 		tier: "UU",
 	},
 	embirch: {
 		inherit: true,
-		isNonstandard: null,
 		tier: "LC",
 	},
 	flarelm: {
 		inherit: true,
-		isNonstandard: null,
 		tier: "NFE",
 	},
 	pyroak: {
 		inherit: true,
-		isNonstandard: null,
 		tier: "RU",
 	},
 	breezi: {
 		inherit: true,
-		isNonstandard: null,
 		tier: "LC",
 	},
 	fidgit: {
 		inherit: true,
-		isNonstandard: null,
 		tier: "OU",
 	},
 	rebble: {
 		inherit: true,
-		isNonstandard: null,
 		tier: "LC",
 	},
 	tactite: {
 		inherit: true,
-		isNonstandard: null,
 		tier: "NFE",
 	},
 	stratagem: {
 		inherit: true,
-		isNonstandard: null,
 		tier: "UU",
 	},
 	privatyke: {
 		inherit: true,
-		isNonstandard: null,
 		tier: "LC",
 	},
 	arghonaut: {
 		inherit: true,
-		isNonstandard: null,
 		tier: "OU",
 	},
 	nohface: {
 		inherit: true,
-		isNonstandard: null,
 		tier: "LC",
 	},
 	kitsunoh: {
 		inherit: true,
-		isNonstandard: null,
 		tier: "UU",
 	},
 	monohm: {
 		inherit: true,
-		isNonstandard: null,
 		tier: "LC",
 	},
 	duohm: {
 		inherit: true,
-		isNonstandard: null,
 		tier: "NFE",
 	},
 	cyclohm: {
 		inherit: true,
-		isNonstandard: null,
 		tier: "UU",
 	},
 	dorsoil: {
 		inherit: true,
-		isNonstandard: null,
 		tier: "LC",
 	},
 	colossoil: {
 		inherit: true,
-		isNonstandard: null,
 		tier: "UU",
 	},
 	protowatt: {
 		inherit: true,
-		isNonstandard: null,
 		tier: "LC",
 	},
 	krilowatt: {
 		inherit: true,
-		isNonstandard: null,
 		tier: "RU",
 	},
 	voodoll: {
 		inherit: true,
-		isNonstandard: null,
 		tier: "LC",
 	},
 	voodoom: {
 		inherit: true,
-		isNonstandard: null,
 		tier: "RU",
 	},
 	scratchet: {
 		inherit: true,
-		isNonstandard: null,
 		tier: "LC",
 	},
 	tomohawk: {
 		inherit: true,
-		isNonstandard: null,
 		tier: "RU",
 	},
 	necturine: {
 		inherit: true,
-		isNonstandard: null,
 		tier: "LC",
 	},
 	necturna: {
 		inherit: true,
-		isNonstandard: null,
 		tier: "OU",
 	},
 	mollux: {
 		inherit: true,
-		isNonstandard: null,
 		tier: "RU",
 	},
 	cupra: {
 		inherit: true,
-		isNonstandard: null,
 		tier: "LC",
 	},
 	argalis: {
 		inherit: true,
-		isNonstandard: null,
 		tier: "NFE",
 	},
 	aurumoth: {
 		inherit: true,
-		isNonstandard: null,
 		tier: "OU",
 	},
 	brattler: {
 		inherit: true,
-		isNonstandard: null,
 		tier: "LC",
 	},
 	malaconda: {
 		inherit: true,
-		isNonstandard: null,
 		tier: "UU",
 	},
 	cawdet: {
 		inherit: true,
-		isNonstandard: null,
 		tier: "LC",
 	},
 	cawmodore: {
 		inherit: true,
-		isNonstandard: null,
 		tier: "UU",
 	},
 	volkritter: {
 		inherit: true,
-		isNonstandard: null,
 		tier: "LC",
 	},
 	volkraken: {
 		inherit: true,
-		isNonstandard: null,
 		tier: "OU",
 	},
 	snugglow: {
 		inherit: true,
-		isNonstandard: null,
 		tier: "LC",
 	},
 	plasmanta: {
 		inherit: true,
-		isNonstandard: null,
 		tier: "UU",
 	},
 	floatoy: {
 		inherit: true,
-		isNonstandard: null,
 		tier: "LC",
 	},
 	caimanoe: {
 		inherit: true,
-		isNonstandard: null,
 		tier: "NFE",
 	},
 	naviathan: {
 		inherit: true,
-		isNonstandard: null,
 		tier: "UU",
 	},
 	crucibelle: {
 		inherit: true,
-		isNonstandard: null,
 		tier: "RU",
 	},
 	crucibellemega: {
 		inherit: true,
-		isNonstandard: null,
 		tier: "OU",
 	},
 	pluffle: {
 		inherit: true,
-		isNonstandard: null,
 		tier: "LC",
 	},
 	kerfluffle: {
 		inherit: true,
-		isNonstandard: null,
 		tier: "RU",
 	},
 	pajantom: {
 		inherit: true,
-		isNonstandard: null,
 		tier: "UU",
 	},
 	mumbao: {
 		inherit: true,
-		isNonstandard: null,
 		tier: "LC",
 	},
 	jumbao: {
 		inherit: true,
-		isNonstandard: null,
 		tier: "OU",
 	},
 	fawnifer: {
 		inherit: true,
-		isNonstandard: null,
 		tier: "LC",
 	},
 	electrelk: {
 		inherit: true,
-		isNonstandard: null,
 		tier: "NFE",
 	},
 	caribolt: {
 		inherit: true,
-		isNonstandard: null,
 		tier: "RU",
 	},
 	smogecko: {
 		inherit: true,
-		isNonstandard: null,
 		tier: "LC",
 	},
 	smoguana: {
 		inherit: true,
-		isNonstandard: null,
 		tier: "NFE",
 	},
 	smokomodo: {
 		inherit: true,
-		isNonstandard: null,
 		tier: "RU",
 	},
 	swirlpool: {
 		inherit: true,
-		isNonstandard: null,
 		tier: "LC",
 	},
 	coribalis: {
 		inherit: true,
-		isNonstandard: null,
 		tier: "NFE",
 	},
 	snaelstrom: {
 		inherit: true,
-		isNonstandard: null,
 		tier: "OU",
 	},
 	justyke: {
 		inherit: true,
-		isNonstandard: null,
 		tier: "LC",
 	},
 	equilibra: {
 		inherit: true,
-		isNonstandard: null,
 		tier: "OU",
 	},
 	solotl: {
 		inherit: true,
-		isNonstandard: null,
 		tier: "LC",
 	},
 	astrolotl: {
 		inherit: true,
-		isNonstandard: null,
 		tier: "UU",
 	},
 	miasmite: {
 		inherit: true,
-		isNonstandard: null,
 		tier: "LC",
 	},
 	miasmaw: {
 		inherit: true,
-		isNonstandard: null,
 		tier: "RU",
 	},
 	chromera: {
 		inherit: true,
-		isNonstandard: null,
 		tier: "RU",
 	},
 	venomicon: {
 		inherit: true,
-		isNonstandard: null,
 		tier: "OU",
 	},
 	venomiconepilogue: {
 		inherit: true,
-		isNonstandard: null,
 		tier: "OU",
 	},
 	saharascal: {
 		inherit: true,
-		isNonstandard: null,
 		tier: "LC",
 	},
 	saharaja: {
 		inherit: true,
-		isNonstandard: null,
 		tier: "UU",
 	},
 	ababo: {
 		inherit: true,
-		isNonstandard: null,
 		tier: "LC",
 	},
 	scattervein: {
 		inherit: true,
-		isNonstandard: null,
 		tier: "NFE",
 	},
 	hemogoblin: {
 		inherit: true,
-		isNonstandard: null,
 		tier: "UU",
 	},
 	cresceidon: {
 		inherit: true,
-		isNonstandard: null,
 		tier: "OU",
 	},
 };
